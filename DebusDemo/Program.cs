9ee34db8967b46ee290132597cd1a212c4a9bcce
Bjarne <buk@topdanmark.dk>
2019-11-14 09:13:50 +0100

    Hej med dig

--- DebusDemo/Program.cs
+++ DebusDemo/Program.cs
@@ -6,7 +6,7 @@ namespace DebugDemo
     {
         static void Main(string[] args)
         {
-            //Prøv det
+            
             Console.WriteLine("Debug demo");
 
             // Variabel
