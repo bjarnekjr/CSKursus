416a83f91283160000b181565968dcb54db4c47d
bjarnekjr <bjarnekjredu@gmail.com>
2019-12-02 15:23:34 +0100

    d

--- Egenskaber/Program.cs
+++ Egenskaber/Program.cs
@@ -6,18 +6,54 @@ namespace Egenskaber
     {
         static void Main(string[] args)
         {
-            Vare v = new Vare();
-            v.Navn = "guld";
-            v.Pris = 100;
-            Console.WriteLine(v.PrisMedMoms());
-            Vare v2 = new Vare("Bronze", 10);
-            Console.WriteLine(v2.PrisMedMoms());
+            //  Vare v = new Vare();
+            //  v.Navn = "guld";
+            //  v.Pris = 100;
+            //  Console.WriteLine(v.PrisMedMoms());
+            //  Vare v2 = new Vare("Bronze", 10);
+            //  Console.WriteLine(v2.PrisMedMoms());
+
+            Person p = new Person();
+            p.Fornavn = "abc";
+            p.Efternavn = "qwer";
+            Console.WriteLine(p.FuldtNavn());
+            Console.WriteLine(p.Fuldtnavn2);
         }
     }
+    class Person
+    {
+        public string Fornavn { get; set; }
 
+        private string _efternavn;
+
+        public string Efternavn
+        {
+            get { return _efternavn; }
+            set { if (value.Length < 4)
+                    _efternavn = "det kan ikke passe med så kort et efternavn";
+            else
+                _efternavn = value; }
+        }
+
+        private string _fuldtnavn2;
+
+        public string Fuldtnavn2
+        {
+            get { return this.Fornavn + " " + this._efternavn; }
+          //  set { _fuldtnavn2 = value; }
+        }
+
+        public string FuldtNavn()
+        {
+            return this.Fornavn + " " + this._efternavn;
+        }
+
+
+    }
     class Vare
     {
         private string _navn ;
+        private double _pris;
 
         public string Navn
         {
@@ -26,9 +62,6 @@ namespace Egenskaber
                 Console.WriteLine("Varenavn sat til: "+ _navn);
             }
         }
-
-        private double _pris;
-
         public double Pris
         {
             get { return _pris; }
---
+++ Terning3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Terning3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Terning t = new Terning();
+            t.Skriv();
+            t.Ryst();
+            t.Vaerdi = 5;
+            t.Skriv();
+            
+        }
+    }
+
+    class Terning
+    {
+        private int _vaerdi;
+        private static Random _rnd = new Random();
+        public int Vaerdi
+        {
+            get { return this._vaerdi; }
+            set
+            {
+                Console.WriteLine("Værdi sættes " + value);
+                if (value < 0 || value > 6)
+                    this._vaerdi = 1;
+                else
+                    this._vaerdi = value;
+                Console.WriteLine("Værdi sat " + this.Vaerdi);
+            }
+        }
+
+        public void Ryst()
+        {
+            this._vaerdi = _rnd.Next(1,7);
+            
+        }
+
+        public void Skriv()
+        {
+            Console.WriteLine("["+this.Vaerdi+"]");
+        }
+
+    }
+}
