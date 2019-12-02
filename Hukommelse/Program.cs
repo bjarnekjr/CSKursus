72ef191a0c7c82bc0b834152e5405197f58a9299
bjarnekjr <bjarnekjredu@gmail.com>
2019-12-02 13:12:46 +0100

    com

---
+++ Hukommelse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Hukommelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person k1 = new Person();
+            Person k2 = new Person();
+
+            k1.Id = 1;
+            k2.Id = 2;
+
+            Console.WriteLine(k1.Id + " " + k2.Id);
+            k1 = k2;
+            Console.WriteLine(k1.Id + " " + k2.Id);
+
+            k1.Id = 3;
+            Console.WriteLine(k1.Id + " " + k2.Id);
+        }
+    }
+
+    struct Person
+    {
+        public int Id;
+    }
+}
---
+++ Klasser/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Klasser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Person");
+
+            Person p1 = new Person("Sigurd", "Jensen", 1960);
+            String navn = p1.FuldtNavn();
+            int alder = p1.Alder();
+            Console.WriteLine(navn + " " + alder.ToString());
+
+        }
+    }
+
+    class Person
+    {
+        public String Fornavn;
+        public String Efternavn;
+        public int Fødselsår;
+
+        public String FuldtNavn()
+        {
+            return this.Fornavn + " " + this.Efternavn;        
+        }
+        public int Alder()
+        {
+            
+            int aar = DateTime.Now.Year;
+            return aar - this.Fødselsår;
+        }
+
+        public Person()
+        {
+            this.Fornavn = "";
+            this.Efternavn = "";
+        }
+        public Person(String fnavn,String enavn, int faar)
+        {
+            if (fnavn == null)
+                Fornavn = "";
+            else Fornavn = fnavn;
+            if (enavn == null)
+                Efternavn = "";
+            else Efternavn = enavn;
+            
+        }
+    }
+}
